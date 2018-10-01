953d3692b226984507f9ffc7f093f05b2e784d7c
DarshanMurthy <darshants.darshan1@gmail.com>
2018-09-30 19:50:17 -0700

    added new permutation

--- src/main/java/com/interview/perfection/Permutation.java
+++ src/main/java/com/interview/perfection/Permutation.java
@@ -1,5 +1,25 @@
 package com.interview.perfection;
 
 public class Permutation {
+    public static void swap(char i, char j){
 
+    }
+
+
+    public static void permutate(String args, int s, int l){
+        if( s == l ){
+
+        }else {
+            for(int i=s; i< l;i++){
+
+
+            }
+
+        }
+    }
+    public static void main(String args[]){
+        String arr="darshan";
+        permutate(arr,0, arr.length());
+
+    }
 }
