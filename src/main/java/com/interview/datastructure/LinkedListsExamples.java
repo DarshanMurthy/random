4176b7fb0d302ec1d3a7eba9138321eb37d6f2e3
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-01 08:13:27 -0700

    linkedlists

---
+++ src/main/java/com/interview/datastructure/LinkedListsExamples.java
@@ -0,0 +1,21 @@
+package com.interview.datastructure;
+
+/*
+Strengths:
+ 1. Fast Operation on the ends
+ 2. Flexible size
+Weakness:
+1. Costly lookups
+
+Uses:
+1. Stacks and Queues only need fast operations on the ends,
+so linkedlists are ideal :)
+ */
+
+
+public class LinkedListsExamples {
+
+    public  static void main(String args[]){
+
+    }
+}
