949b41418d389951a401603e37717bdd52f5c698
DarshanMurthy <darshants.darshan1@gmail.com>
2018-09-30 18:01:49 -0700

    length is a method

--- src/main/java/com/interview/java_related/StringConfusion.java
+++ src/main/java/com/interview/java_related/StringConfusion.java
@@ -1,4 +1,9 @@
 package com.interview.java_related;
 
 public class StringConfusion {
+    public static void main(String args[]){
+        String value= "murthy";
+        // Length() is a method :)
+        System.out.println(value.length());
+    }
 }
