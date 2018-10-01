eefbd7efc4b1809226bda7e806cf14321efc8e81
DarshanMurthy <darshants.darshan1@gmail.com>
2018-09-30 18:11:19 -0700

    "added Arrays.slice"

--- src/main/java/com/interview/java_related/arraySlicing.java
+++ src/main/java/com/interview/java_related/arraySlicing.java
@@ -5,9 +5,18 @@ import java.util.Arrays;
 public class arraySlicing {
 
     public static void main(String args[]){
+
+        /**
+         * Don't include the last element :)
+         */
         int arr[] = {12,3,4,5};
         for(int i :Arrays.copyOfRange(arr,1,3)){
             System.out.print(i);
+        }
+
+        int arr2[] = {1,2,3,4};
+        for( int i : Arrays.copyOfRange(arr2,0,1)){
+            System.out.println(i);
         }
 
     }
