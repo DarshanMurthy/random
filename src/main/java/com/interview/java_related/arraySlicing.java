86b4dad7a50de05f8ddb6f9fa9c3e2ab3e9f154b
DarshanMurthy <darshants.darshan1@gmail.com>
2018-09-30 18:14:46 -0700

    array slicing

--- src/main/java/com/interview/java_related/arraySlicing.java
+++ src/main/java/com/interview/java_related/arraySlicing.java
@@ -1,5 +1,6 @@
 package com.interview.java_related;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class arraySlicing {
@@ -7,17 +8,13 @@ public class arraySlicing {
     public static void main(String args[]){
 
         /**
-         * Don't include the last element :)
+         * copyOfRange basically copies
+         * elements from "from" to "to-1" indexed
+         * elements.
          */
         int arr[] = {12,3,4,5};
         for(int i :Arrays.copyOfRange(arr,1,3)){
             System.out.print(i);
         }
-
-        int arr2[] = {1,2,3,4};
-        for( int i : Arrays.copyOfRange(arr2,0,1)){
-            System.out.println(i);
-        }
-
     }
 }
