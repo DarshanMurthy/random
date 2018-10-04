51be6e7495f7556953d326fab640f30f83289e04
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-04 07:14:29 -0700

    head linkedlist

--- src/main/java/com/interview/datastructure/Lists/ListImplementation.java
+++ src/main/java/com/interview/datastructure/Lists/ListImplementation.java
@@ -2,7 +2,18 @@ package com.interview.datastructure.Lists;
 
 public class ListImplementation {
     public static void main(String args[]){
-        Node head = new Node();
-        
+        Node head = new Node(0);
+        Node one = new Node(10);
+        Node two = new Node(20);
+        head.next = one;
+        one.next = two;
+
+        // Traverse
+        Node cur = head.next;
+        while (cur!=null) {
+            System.out.println(cur.data);
+            cur = cur.next;
+        }
+
     }
 }
--- src/main/java/com/interview/datastructure/Lists/Node.java
+++ src/main/java/com/interview/datastructure/Lists/Node.java
@@ -3,4 +3,7 @@ package com.interview.datastructure.Lists;
 public class Node {
     int data;
     Node next;
+    Node(int data){
+        this.data =data;
+    }
 }
