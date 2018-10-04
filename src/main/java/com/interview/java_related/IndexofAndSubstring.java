e94f781dad447db8869c42dec210e08b8a8fa412
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-04 08:48:11 -0700

    indexof in string

---
+++ src/main/java/com/interview/java_related/IndexofAndSubstring.java
@@ -0,0 +1,18 @@
+package com.interview.java_related;
+
+public class IndexofAndSubstring {
+    public  static void main(String args[]){
+        String pre="godisgreat";
+        String str = "god";
+        System.out.println(str.indexOf(pre));
+        //System.out.println(pre.substring(0,pre.length()-1));
+
+        String test1="value";
+        String test2="tempvaluenotes";
+        System.out.println(test1.indexOf(test2));
+        System.out.println(test2.indexOf(test1));
+
+        String test3="darshan";
+        System.out.println(test3.indexOf('a'));
+    }
+}
