7cbdd1d3ad243288e75550d18c2d471d7ed9af05
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-08 08:16:53 -0700

    added few comparator

--- src/main/java/com/interview/java_related/comparator_examples/MainExamples.java
+++ src/main/java/com/interview/java_related/comparator_examples/MainExamples.java
@@ -2,6 +2,7 @@ package com.interview.java_related.comparator_examples;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 class Student{
     int rollno;
@@ -9,18 +10,41 @@ class Student{
     public Student(int rollno, String name, String address){
         this.rollno = rollno;
         this.address = address;
+        this.name = name;
+    }
+}
+
+class Sortbyroll implements Comparator<Student>{
+    public int compare(Student a, Student b){
+        return  a.rollno-b.rollno;
+    }
+
+}
+
+class Sortbyname implements Comparator<Student>{
+    public int compare(Student a, Student b){
+        return a.name.compareTo(b.name);
     }
 }
 
 public class MainExamples {
     public static void main(String [] main){
         ArrayList<Student> ar = new ArrayList<>();
+        ar.add(new Student(14,"note","london3"));
         ar.add(new Student(11, "tom","london1"));
         ar.add(new Student(12,"tim","london2"));
 
-        for(int i=0;i< ar.size();i++){
-            System.out.println(ar.get(i));
+        for(int i=0;i< ar.size();i++) {
+            System.out.println(ar.get(i).name);
         }
-        Collections.sort(ar, new);
+
+        Collections.sort(ar,new Sortbyname());
+
+        for(int i=0;i< ar.size();i++) {
+            System.out.println(ar.get(i).name);
+        }
+
+
+
     }
 }
