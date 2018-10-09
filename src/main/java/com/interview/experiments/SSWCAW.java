9f59118f2de4d1c5a7177dc371d1966d381d85b2
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-09 09:03:27 -0700

    almost there

--- src/main/java/com/interview/experiments/SSWCAW.java
+++ src/main/java/com/interview/experiments/SSWCAW.java
@@ -1,5 +1,6 @@
 package com.interview.experiments;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SSWCAW {
@@ -8,12 +9,23 @@ public class SSWCAW {
         String [] words = {"foo","bar"};
         HashMap<Character, String> map =
                 new HashMap<>();
+        int wordsLen=0;
         for( String word: words){
             map.put(word.charAt(0),word);
+            wordsLen+=word.length();
         }
         int len = s.length();
+        int counter=0;
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i=0;i< len;i++){
-            
+            while (map.containsKey(s.charAt(i)) && counter!=words.length){
+                counter ++;
+                i += map.get(s.charAt(i)).length();
+            }
+            if(counter == words.length){
+                list.add(i-wordsLen);
+                counter=0;
+            }
         }
     }
 }
