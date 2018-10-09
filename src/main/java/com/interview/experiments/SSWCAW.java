27264c1342427e344d70101dd811236fbc848d6d
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-09 08:46:07 -0700

    added linkedlist

--- src/main/java/com/interview/experiments/SSWCAW.java
+++ src/main/java/com/interview/experiments/SSWCAW.java
@@ -1,11 +1,19 @@
 package com.interview.experiments;
 
+import java.util.HashMap;
+
 public class SSWCAW {
     public static void main(String args[]){
-        System.out.println("hello");
-
         String s = "barfoothefoobarman";
         String [] words = {"foo","bar"};
-
+        HashMap<Character, String> map =
+                new HashMap<>();
+        for( String word: words){
+            map.put(word.charAt(0),word);
+        }
+        int len = s.length();
+        for(int i=0;i< len;i++){
+            
+        }
     }
 }
