36a352ecbe5d4dbb18645664d0053dca98d4288d
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-12 07:50:58 -0700

    substrings

--- src/main/java/com/interview/experiments/LongestSubstringWithKMostDistictCharacters.java
+++ src/main/java/com/interview/experiments/LongestSubstringWithKMostDistictCharacters.java
@@ -1,8 +1,19 @@
 package com.interview.experiments;
 
+import java.util.HashMap;
+
 public class LongestSubstringWithKMostDistictCharacters {
 
     public static void main(String args[]){
-        
+        HashMap<Integer, Character> map = new HashMap<>();
+        String arr= "valuesstring";
+        for(int i=0;i< arr.length();i++){
+            for(int j=i+1;j< arr.length();j++){
+                System.out.print(arr.substring(i,j));
+                System.out.println();
+            }
+            System.out.println();
+        }
+
     }
 }
