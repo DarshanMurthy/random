961bd9e8f71bf51290a73cbc738f55cf57d35bf3
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-12 07:39:33 -0700

    two characters

---
+++ src/main/java/com/interview/experiments/LongestSubstringWithKMostDistictCharacters.java
@@ -0,0 +1,8 @@
+package com.interview.experiments;
+
+public class LongestSubstringWithKMostDistictCharacters {
+
+    public static void main(String args[]){
+        
+    }
+}
--- src/main/java/com/interview/revisions/substringexample.java
+++ src/main/java/com/interview/revisions/substringexample.java
@@ -7,16 +7,13 @@ import java.util.List;
 public class substringexample {
     public static void main(String args[]){
         String s = "barfoothefoobarman";
-        String words[]={"bar", "foo"};
+        String words[]={"arf", "oot"};
         int len = words[0].length();
-        
         for(int j=0; j<len; j++) {
             for (int i = j; i <= s.length() - len; i = i + len) {
                 String sub = s.substring(i, i+len);
-                System.out.print(sub);
-                System.out.print(" ");
             }
-            System.out.println();
+
         }
 
 
