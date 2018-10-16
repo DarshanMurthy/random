82db6b34e55daf9e39049879c73675c3e8fb978e
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-16 08:11:16 -0700

    two for loops for cutting string

--- src/main/java/com/interview/revisions/example.java
+++ src/main/java/com/interview/revisions/example.java
@@ -4,16 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class example {
-
-
     public static void main(String arsgs[]){
-        String s="barfo" ;
-        String [] words={"foo", "bar"};
+        String s = "barfo";
+        String words[] ={ "bar", "foo"};
         int len = words[0].length();
+
         for(int j=0;j< len;j++){
-            for(int i=j;i< s.length()-len;i=i+len){
-                String sub = s.substring(i, i+len);
-                System.out.println(sub);
+            for(int i=j;i<s.length()-len;i=i+len ){
+                System.out.printf(" %d --> %d ",i,i+len);
+                System.out.println(s.substring(i,i+len));
             }
             System.out.println();
         }
