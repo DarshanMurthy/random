92eaa5b59a603a5246e168caec05998717f717f2
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-16 08:19:12 -0700

    short break

--- src/main/java/com/interview/revisions/example.java
+++ src/main/java/com/interview/revisions/example.java
@@ -5,20 +5,16 @@ import java.util.Map;
 
 public class example {
     public static void main(String arsgs[]){
-        String s = "barfo";
-        String words[] ={ "bar", "foo"};
-        int len = words[0].length();
-
-        for(int j=0;j< len;j++){
-            for(int i=j;i<s.length()-len;i=i+len ){
-                System.out.printf(" %d --> %d ",i,i+len);
-                System.out.println(s.substring(i,i+len));
-            }
-            System.out.println();
-        }
-
-
-
-
+       String s = "barfoothefoobarman";
+       String words[] ={"bar","foo"};
+       int len = words[0].length();
+       for(int j=0;j< len;j++){
+           for(int i=j; i<=s.length()-len; i=i+len){
+               System.out.print(s.substring(i,i+len));
+               System.out.printf("%d --> %d",j,i);
+               System.out.println();
+           }
+           System.out.println();
+       }
     }
 }
