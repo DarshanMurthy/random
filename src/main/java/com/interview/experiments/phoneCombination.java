ac4e56548285993e4aca5fb9413fb8f5100c51bd
DarshanMurthy <darshants.darshan1@gmail.com>
2018-10-22 07:19:02 -0700

    added combination problem

--- src/main/java/com/interview/experiments/phoneCombination.java
+++ src/main/java/com/interview/experiments/phoneCombination.java
@@ -1,14 +1,36 @@
 package com.interview.experiments;
 
+import java.util.LinkedList;
+
 public class phoneCombination {
+    public static final String[] KEYS = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    public static void combinationPrint(String digits,
+                                        String prefix,
+                                        LinkedList<String> list,
+                                        int level){
+        if(level == prefix.length()){
+            list.add(prefix);
+            return;
+        }
+
+        String letters = KEYS[digits.charAt(level)-'0'];
+        for(int i=0;i< letters.length();i++){
+            combinationPrint(digits, prefix+letters.charAt(i),list,level+1 );
+
+        }
+    }
 
     public static void main(String args[]){
-        final String[] KEYS = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-        String digits="25987";
+        String digits="23";
+        int level =0;
+        String prefix="";
+        LinkedList<String> list = new LinkedList<>();
+        combinationPrint(digits,prefix,list,level);
+        for(String i: list){
+            System.out.println(i);
 
-        for(int i=0;i< digits.length();i++){
-            System.out.println(KEYS[(digits.charAt(i)-'0')]);
         }
+
 
 
 
