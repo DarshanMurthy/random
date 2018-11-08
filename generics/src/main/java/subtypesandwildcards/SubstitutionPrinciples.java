d5131027903e864e306e852ba5249c3088dbe890
DarshanMurthy <darshants.darshan1@gmail.com>
2018-11-08 05:52:52 -0800

    new tests

--- generics/src/main/java/subtypesandwildcards/SubstitutionPrinciples.java
+++ generics/src/main/java/subtypesandwildcards/SubstitutionPrinciples.java
@@ -25,4 +25,38 @@ public class SubstitutionPrinciples {
         System.out.print(list.toString());
         assert list.toString().equals("[1, 3]");
     }
+
+    @Test
+    public void substitutionFalacy(){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        /**
+         * Error:(36, 29) java: incompatible types: java.util.List<java.lang.Number> cannot be converted to java.util.List<java.lang.Integer>
+         */
+        //List<Number> its = list;
+        //its.add(2.9);
+
+    }
+
+    public void otherSubstitutionExample(){
+        List<Number> list = new ArrayList<>();
+        list.add(10);
+        list.add(11);
+        /**
+         * Incompatible types
+         */
+        //List<Integer> itms = list;
+    }
+
+    public  void voidSubstitutionExample2(){
+        //List<Number> list = new ArrayList<Integer>();
+        /**
+         * Incompatible types
+         */
+    }
+
+
+
+
 }
