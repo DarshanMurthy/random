148de4748add09b78191598b0fb77818d063488e
DarshanMurthy <darshants.darshan1@gmail.com>
2019-01-03 08:41:21 -0800

    vowels and consonents

---
+++ src/main/java/com/interview/java_related/introduction/checkvowelconsonents.java
@@ -0,0 +1,14 @@
+package com.interview.java_related.introduction;
+
+public class checkvowelconsonents {
+    public static void main(String args[]){
+        // Choose vowels and Consonants.
+        char letters='i';
+        if(letters == 'a'||letters=='e'||letters == 'i'|| letters == 'o'||letters=='u'){
+            System.out.println("vowels");
+        }else{
+            System.out.println("consonents");
+        }
+
+    }
+}
